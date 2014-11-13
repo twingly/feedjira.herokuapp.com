7a3da7b4a933ec8583f9b6fa612a5c3b489d7445
Patrik Ragnarsson <patrik@starkast.net>
2014-11-13 16:59:43 +0100

    support not only Atom feeds

--- monkey.rb
+++ monkey.rb
@@ -3,12 +3,9 @@ require "jsonable"
 
 module Feedjira
   module Parser
-    class Atom
-      include Jsonable
-    end
-
-    class AtomEntry
-      include Jsonable
-    end
+    class Atom;       include Jsonable; end
+    class AtomEntry;  include Jsonable; end
+    class RSS;        include Jsonable; end
+    class RSSEntry;   include Jsonable; end
   end
 end
