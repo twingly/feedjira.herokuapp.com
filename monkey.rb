a47151e4e816f47c0cf1c99ba7c1797e91c9a239
Patrik Ragnarsson <patrik@starkast.net>
2014-11-14 13:55:32 +0100

    support all feeds

--- monkey.rb
+++ monkey.rb
@@ -1,11 +1,6 @@
 require "feedjira"
 require "jsonable"
 
-module Feedjira
-  module Parser
-    class Atom;       include Jsonable; end
-    class AtomEntry;  include Jsonable; end
-    class RSS;        include Jsonable; end
-    class RSSEntry;   include Jsonable; end
-  end
+Feedjira::Parser.constants.each do |constant|
+  Feedjira::Parser.const_get(constant).class_exec { include Jsonable }
 end
